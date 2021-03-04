70eb5bda1fe8fbbd13742db4b9d547f563df52a9
Madeleine Glindorf <mglindorf@gmail.com>
2021-03-04 18:54:22 +0100

    Initial commit

---
+++ OblOpg1/Beer.cs
@@ -0,0 +1,87 @@
+﻿using System;
+
+namespace OblOpg1
+{
+    public class Beer
+    {
+        /// <summary>
+        /// Initialisering
+        /// </summary>
+
+        private string _name;
+        private double _price;
+        private double _abv;
+
+        /// <summary>
+        /// Her er to constructore - den anden af dem (uden parametre) er til brug i opg. 4
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="name"></param>
+        /// <param name="price"></param>
+        /// <param name="abv"></param>
+
+        public Beer(int id, string name, double price, double abv)
+        {
+            Id = id;
+            Name = name;
+            Price = price;
+            Abv = abv;
+        }
+
+        public Beer()
+        { }
+
+        /// <summary>
+        /// Property for Id
+        /// </summary>
+
+        public int Id { get; set; }
+
+        /// <summary>
+        /// Property for Name. Navnet skal være på mindst fire characters, ellers kastes en Exception.
+        /// </summary>
+
+        public string Name
+        {
+            get => _name;
+            set
+            {
+                if (value.Length < 4) throw new ArgumentException();
+                _name = value;
+            }
+        }
+
+        /// <summary>
+        /// Property for Price. Prisen skal være højere end 0 kr, ellers kastes en Exception.
+        /// </summary>
+
+        public double Price
+        {
+            get => _price;
+            set
+            {
+                if (value < 1) throw new ArgumentOutOfRangeException();
+                _price = value;
+            }
+        }
+
+        /// <summary>
+        /// Property for Abv (Alcohol by volume). Den skal være mellem 0 og 100, ellers kastes en Exception.
+        /// </summary>
+
+        public double Abv
+        {
+            get => _abv;
+            set
+            {
+                if (value < 0 || value > 100) throw new ArgumentOutOfRangeException();
+                _abv = value;
+            }
+        }
+
+        public override string ToString()
+        {
+            return $"Id number {Id}, Name: {Name}. Alcohol by volume is: {Abv} and the price is: {Price}."; 
+        }
+    }
+}
---
+++ OblOpg1Tests/BeerTests.cs
@@ -0,0 +1,73 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using OblOpg1;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OblOpg1.Tests
+{
+    [TestClass()]
+    public class BeerTests
+    {
+        private Beer beer;
+
+        [TestInitialize]
+        public void init()
+        {
+            beer = new Beer(1, "RoastBeer", 12.0, 15.3);
+        }
+
+        /// <summary>
+        /// Her testes Id 
+        /// </summary>
+
+        [TestMethod]
+        public void IdTest()
+        {
+            Assert.AreEqual(1, beer.Id);
+            beer.Id = 2;
+            Assert.AreEqual(2, beer.Id);
+        }
+
+        /// <summary>
+        /// Her testes navn. Det skal være på mindst fire characters, derfor tester jeg, at den kaster en exception ved kun tre.
+        /// </summary>
+
+        [TestMethod]
+        public void NameTest()
+        {
+            Assert.AreEqual("RoastBeer", beer.Name);
+            beer.Name = "Abcd";
+            Assert.AreEqual("Abcd", beer.Name);
+            Assert.ThrowsException<ArgumentException>(() => beer.Name = "Abc");
+        }
+
+        /// <summary>
+        /// Her testet Price. Prisen skal være over 0,-. Derfor tester jeg 0 og -11, der begge er uden for det tilladte interval.
+        /// </summary>
+
+        [TestMethod]
+        public void PriceTest()
+        {
+            beer.Price= 12;
+            Assert.AreEqual(12, beer.Price);
+            beer.Price = 111.5;
+            Assert.AreEqual(111.5, beer.Price);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer.Price = 0);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer.Price = -11);
+        }
+
+        /// <summary>
+        /// Her tester jeg Abv (Alcohol by volume). Værdien skal være mellem 0 og 100, ellers kastes en Exception, derfor testes -1 og 101, der begge ligger lige uden for intervallet, der er tilladt.
+        /// </summary>
+
+        [TestMethod]
+        public void AbvTest()
+        {
+            beer.Abv = 12.2;
+            Assert.AreEqual(12.2, beer.Abv);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer.Abv = -1);
+            Assert.ThrowsException<ArgumentOutOfRangeException>(() => beer.Abv = 101);
+        }
+    }
+}
